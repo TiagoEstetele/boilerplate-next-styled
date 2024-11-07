6facc16c5c442fadbba43e8d14adea5235bd1e19
Tiago Estetele <tiago.estetele@okngroup.com.br>
2024-11-06 23:26:24 -0300

    Add styled components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next'
 import localFont from 'next/font/local'
-import './globals.css'
+import StyledComponentsRegistry from './lib/registry'
 
 const geistSans = localFont({
   src: './fonts/GeistVF.woff',
@@ -28,7 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
       </body>
     </html>
   )
