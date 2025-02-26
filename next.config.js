9aa7f202fa9e375a4aedad0bda68b6480955ff33
Tiago Estetele <tiago.estetele@okngroup.com.br>
2025-02-25 22:41:27 -0300

    fix. eslint dirs

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 // next.config.js (CommonJS)
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Sua configuração aqui
+  eslint: {
+    dirs: ['app', 'components', 'styles'] // Only run ESLint on the 'pages' and 'utils' directories during production builds (next build)
+  }
 }
 
 module.exports = nextConfig
